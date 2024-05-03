ae579aff90c0da0086d04a4b4271bc70a38f7cde
Rachelle DeMan <demanr@mcmaster.ca>
2024-05-03 19:08:06 -0400

    chore: fixing footer

--- src/components/CopyrightBeta.tsx
+++ src/components/CopyrightBeta.tsx
@@ -5,9 +5,9 @@ const CopyrightBeta = () => {
   let year = start.getFullYear().toString();
 
   return (
-    <footer className="bottom-0 w-full py-6 text-xs text-center sm:absolute">
+    <div className="bottom-0 w-full py-6 text-xs text-center sm:absolute">
       © Rachelle De Man {year} - All Rights Reserved.
-    </footer>
+    </div>
   );
 };
 
