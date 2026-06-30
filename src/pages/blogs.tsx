68af5438ee11bfad3d0b672631880a449c4b493b
Rachelle DeMan <demanr@mcmaster.ca>
2026-06-30 13:49:10 -0400

    feat: cosmetic fixes

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -7,7 +7,7 @@ export default function Blogs() {
   return (
     <div className="flex flex-col w-full min-h-screen">
       <Navbar />
-      <div className="flex-1 px-8 py-8">
+      <div className="flex-1 px-4 py-4 md:py-8 md:px-8">
         <div className="max-w-4xl px-2">
           {/* <Link
             href="/"
@@ -16,18 +16,18 @@ export default function Blogs() {
             ← Back
           </Link> */}
           <h1 className="text-xl md:text-3xl">Rachelle-Blogs</h1>
-          <p className="w-full max-w-2xl py-2 text-lg leading-5 border-b md:leading-6 md:text-xl opacity-60">
+          <p className="w-full max-w-2xl pb-2 text-base leading-5 border-b md:py-2 md:leading-6 md:text-xl opacity-60">
             Sharing some of what I learned on the journey to becoming a
             developer
           </p>
-          <div className="flex flex-col gap-4 pt-8">
+          {/* remove once live */}
+          {/* <p className="pt-4 text-lg md:text-xl">Coming Soon!</p> */}
+
+          <div className="flex flex-col gap-4 pt-4 md:pt-8">
             {blogPosts.map((post) => (
-              <article
-                key={post.slug}
-                className=""
-              >
-                <div className="grid grid-cols-[auto_auto_1fr] gap-x-2 text-lg">
-                  <span className="w-[115px] opacity-50">
+              <article key={post.slug} className="">
+                <div className="grid grid-cols-[auto_auto_1fr] gap-x-2 text-base md:text-lg">
+                  <span className="w-[105px] md:w-[115px] opacity-50">
                     {post.date}
                   </span>
                   <span className="opacity-50 ">–</span>
@@ -37,8 +37,8 @@ export default function Blogs() {
                   >
                     {post.title}
                   </Link>
-                  <span className="col-span-2"></span>
-                  <p className="text-base leading-5 md:leading-6 opacity-60">
+                  <span className="md:col-span-2"></span>
+                  <p className="hidden text-base leading-5 md:block md:leading-6 opacity-60">
                     {post.description}
                   </p>
                 </div>
